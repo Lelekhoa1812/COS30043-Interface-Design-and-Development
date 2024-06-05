9f4b2c093ad10775e7080c522d778e771f7f924f
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-06-05 11:37:37 +1000

    Add files via upload

---
+++ Lab6/Source code/Week 6 Sample Code/simple form.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue
+const app = createApp({
+	data() {
+		return {
+			fName: '', 
+			lName:'', 
+			errors: []
+		}
+	},
+	methods: {
+		checkForm: function(e){
+			var result = true
+			this.errors = []
+			if (!this.fName.trim()) {
+				result = false
+				this.errors.push ("Please enter first name.")		
+			}
+			if (!this.lName.trim()) {
+				result = false
+				this.errors.push ("Please enter last name.")		
+			}
+			if (!result)
+				e.preventDefault()// prevent form submission			
+		}
+
+	}
+})
+app.mount('#app')
